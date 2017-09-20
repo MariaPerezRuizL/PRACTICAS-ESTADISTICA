4fbce018d2537c305b20f3ef58514ba71ec91583
MariaPerezRuizL <perezeruizmaria@gmail.com>
2017-09-20 22:18:15 +0200

    v final

--- hitori.R
+++ hitori.R
@@ -1,4 +1,4 @@
-# Capítulo 3. 
+#Capítulo 3. 
 # UN EJEMPLO PARA APRENDER R. EL HITORI
 
 # FUNCIÓN QUE DIBUJA EL TABLERO
@@ -15,7 +15,7 @@ dibujatablero<-function(hitori,tachada){
   
   for(i in 1:6){
     for(j in 1:6){
-      text(j-0.5, 7-i-0.5, hitor[i,j], col=1, cex=2)
+      text(j-0.5, 7-i-0.5, hitori[i,j], col=1, cex=2)
       if(tachada[i,j]==1){
         text(j-0.5, 7-i-0.5, "X", cex=5, col=2)
       }
@@ -23,7 +23,6 @@ dibujatablero<-function(hitori,tachada){
   }
   
 }
-
 
 # FUNCIÓN PRINCIPAL
 
@@ -38,15 +37,16 @@ juega.hitori<-function(){
   #v2.Leo en un vector
   #hitori<-scan("/Users/Maria/Documents/Pruebas/hitori.rtf", sep="")
   
-  #Lo pasamos a matriz
+  #Lo pasamos a matrix
   #hitori<-as.matrix(hitori)
   
-  #Creo la matriz del hitori de prueba 6x6
+  #Creo la matrix del hitori de prueba 6x6
   num<-c(2,2,4,3,4,1,6,5,5,1,1,3,3,5,6,1,4,3,5,1,1,1,6,2,3,4,3,2,5,1,4,3,6,6,2,5)
   
-  hitori<-as.matrix(num, nrow= 6,ncol=6)
-  #Creamos una matriz que representa las casillas tachadas llena de ceros
+  hitori<-matrix(num, nrow= 6,ncol=6)
+  #Creamos una matrix que representa las casillas tachadas llena de ceros
   tachada<-matrix(0, nrow=6, ncol=6)
+  
   
   #Dibujamos el tablero con las dos matrices con la función de dibujar
   dibujatablero(hitori, tachada)
@@ -74,8 +74,8 @@ juega.hitori<-function(){
       # Caso jugamos=1: TACHAR CASILLA
       if(jugamos==1){
         
-        #Miramos si la casilla está tachada: recorremos la matriz si tiene 1 para tachada
-        if(tachada(pos[1], pos[2])==1){
+        #Miramos si la casilla está tachada: recorremos la matrix si tiene 1 para tachada
+        if(tachada[pos[1], pos[2]]==1){
           cat("La posición elegida ya estaba tachada\n")
         }
         else 
@@ -86,7 +86,7 @@ juega.hitori<-function(){
           if (pos[1]==1){
             mira.fila=2
           }
-          else if(pos[6]==1){
+          else if(pos[1]==6){
             mira.fila=5
           }
           else{
@@ -103,7 +103,7 @@ juega.hitori<-function(){
           }
           else{
             #Creo un vector con las columnas izq y dcha
-            miro.columna=c(pos[2]-1, pos[2]+1)
+            mira.columna=c(pos[2]-1, pos[2]+1)
           }
           
           #Comprobamos que las casillas no estén tachadas
@@ -130,7 +130,7 @@ juega.hitori<-function(){
         }
       }
       #dibujamos otra vez el tablero con el cambio realizado en cada jugada
-      dibujatablero(hitori, tachada)
+      dibujatablero(hitori,tachada)
       
       #Vamos mirando si el juego se ha acabado o no
       #Creo un vector numérico vacio para filas y columnas
@@ -138,10 +138,10 @@ juega.hitori<-function(){
       fila<-numeric()
       col<-numeric()
       
-      #Recorremos la matriz
+      #Recorremos la matrix
       for(i in 1:6) {
         fila[i]=sum(table(hitori[i, tachada[i, ]==0])>1)
-        col[i]=sum(table(hitori[tachada[,i]==0,i])>1)
+        col[i]=sum(table(hitori[tachada[ ,i]==0,i])>1)
       }
       
       if(sum(fila,col)>0){
@@ -159,4 +159,3 @@ juega.hitori<-function(){
     
   }
 }
-
